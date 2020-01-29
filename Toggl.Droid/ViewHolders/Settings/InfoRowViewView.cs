617bdfa66cd3ca36b9d141c42bb2cf712ccf4e42
Daivid S. Silverio <daivid.silverio@gmail.com>
2020-01-29 11:23:13 -0400

    🚧 Revamp droid settings (#6954)
    * :construction: Move ISettings models to shared layer
    
    * :construction: Replace notifications methods with view actions
    
    * :construction: Extract setting rows layouts
    
    * :sparkles: Add viewholders for settings rows
    
    * :construction: Dinamically inflate settings rows
    
    * :sparkles: Add headers and dividers rows
    
    * :see_no_evil: Add notification settings rows
    
    * 🙈 Fix stopped timer notification settings label
    
    Co-Authored-By: Juan Laube <juanlaube@users.noreply.github.com>
    
    * :bug: Handle switch clicks on toggle rows
    
    * :construction: Reduce some of the code duplication
    
    * :green_heart:
    
    Co-authored-by: Juan Laube <juanlaube@users.noreply.github.com>

---
+++ Toggl.Droid/ViewHolders/Settings/InfoRowViewView.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Reactive.Linq;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Toggl.Core.UI.Views.Settings;
+
+namespace Toggl.Droid.ViewHolders.Settings
+{
+    public class InfoRowViewView : SettingsRowView<InfoRow>
+    {
+        public static InfoRowViewView Create(Context context)
+            => new InfoRowViewView(LayoutInflater.From(context).Inflate(Resource.Layout.SettingsInfoRowView, null));
+
+        private TextView title;
+        private TextView description;
+        
+        public InfoRowViewView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        private InfoRowViewView(View itemView) : base(itemView)
+        {
+            title = ItemView.FindViewById<TextView>(Resource.Id.Title);
+            description = ItemView.FindViewById<TextView>(Resource.Id.Description);
+        }
+
+        protected override void OnRowDataChanged()
+        {
+            title.Text = RowData.Title;
+            description.Text = RowData.Detail;
+        }
+
+        public IDisposable Bind(IObservable<string> descriptionProvider, string presetTitle)
+            => descriptionProvider
+                .Select(detail => new InfoRow(presetTitle, detail))
+                .Subscribe(SetRowData);
+    }
+}
