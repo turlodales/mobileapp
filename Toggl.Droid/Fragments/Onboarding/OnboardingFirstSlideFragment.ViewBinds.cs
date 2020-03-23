c08862334fc99d577afdc8f2971a85fb0cc5ef39
Daivid S. Silverio <daivid.silverio@gmail.com>
2020-03-23 11:16:09 -0400

    Apply onboarding feedback (#7220)
    * :triangular_ruler: Apply 4:7.2 magic aspect ratio for the toggl man
    
    * :art: Set the sytem navigation bar color to the bg color
    
    * :straight_ruler: Fix first slide line spacing
    
    * :lipstick:
    
    * :hack: Use a second SurfaceView with a paused video to act as a background
    
    * Use the togglman-less vid for the background
    
    * Re-encode the videos so they work on old APIs
    
    * Set proper z ordering on surfaces
    
    * Use a textureview
    
    * :construction: Replace togglman mp4 with static images
    
    Co-authored-by: peter <53510080+pdrj@users.noreply.github.com>

--- Toggl.Droid/Fragments/Onboarding/OnboardingFirstSlideFragment.ViewBinds.cs
+++ Toggl.Droid/Fragments/Onboarding/OnboardingFirstSlideFragment.ViewBinds.cs
@@ -6,46 +6,14 @@ using Android.Widget;
 
 namespace Toggl.Droid.Fragments.Onboarding
 {
-    public partial class OnboardingFirstSlideFragment : ISurfaceHolderCallback
+    public partial class OnboardingFirstSlideFragment
     {
         private TextView onboardingMessage;
-        private SurfaceView onboardingSurfaceView;
-
-        private MediaPlayer mediaPlayer;
-
 
         protected override void InitializeViews(View view)
         {
             onboardingMessage = view.FindViewById<TextView>(Resource.Id.message);
-            onboardingSurfaceView = view.FindViewById<SurfaceView>(Resource.Id.togglMan);
-
-            onboardingSurfaceView.Holder.AddCallback(this);
             onboardingMessage.Text = Shared.Resources.OnboardingMessagePage1;
-        }
-        public void SurfaceChanged(ISurfaceHolder holder, Format format, int width, int height)
-        {
-        }
-
-        public void SurfaceCreated(ISurfaceHolder holder)
-        {
-            mediaPlayer?.Release();
-            mediaPlayer = MediaPlayer.Create(Application.Context, Resource.Raw.togglman);
-            mediaPlayer.SetVideoScalingMode(VideoScalingMode.ScaleToFitWithCropping);
-            mediaPlayer.SetDisplay(holder);
-            mediaPlayer.Start();
-            mediaPlayer.Looping = true;
-        }
-
-        public void SurfaceDestroyed(ISurfaceHolder holder)
-        {
-        }
-
-        public override void OnDestroy()
-        {
-            base.OnDestroy();
-            mediaPlayer?.Release();
-            onboardingSurfaceView?.Holder?.RemoveCallback(this);
-            mediaPlayer = null;
         }
     }
 }
