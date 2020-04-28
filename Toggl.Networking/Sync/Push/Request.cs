9dd4cbced41e5af2da0adc2ded45d7d065b51bf8
Kornelije Petak <kornelije@gmail.com>
2020-04-28 12:10:15 +0200

    💔 Break down partial classes

--- Toggl.Networking/Sync/Push/Request.cs
+++ Toggl.Networking/Sync/Push/Request.cs
@@ -19,5 +19,65 @@ namespace Toggl.Networking.Sync.Push
 
         public UpdateAction<IPreferences> Preferences { get; set; }
         public UpdateAction<IUser> User { get; set; }
+
+        public void CreateTimeEntries(IEnumerable<ITimeEntry> timeEntries)
+        {
+            timeEntries
+                .Select(timeEntry => new TimeEntry(timeEntry))
+                .Select(timeEntry => new CreateAction<TimeEntry>(timeEntry))
+                .AddTo(TimeEntries);
+        }
+
+        public void UpdateTimeEntries(IEnumerable<ITimeEntry> timeEntries)
+        {
+            timeEntries
+                .Select(timeEntry => new TimeEntry(timeEntry))
+                .Select(timeEntry => new UpdateAction<TimeEntry>(timeEntry))
+                .AddTo(TimeEntries);
+        }
+
+        public void DeleteTimeEntries(IEnumerable<ITimeEntry> timeEntries)
+        {
+            timeEntries
+                .Select(timeEntry => new TimeEntry(timeEntry))
+                .Select(timeEntry => new DeleteAction(timeEntry.Id, timeEntry.WorkspaceId))
+                .AddTo(TimeEntries);
+        }
+
+        public void CreateProjects(IEnumerable<IProject> projects)
+        {
+            projects
+                .Select(project => new Project(project))
+                .Select(project => new CreateAction<Project>(project))
+                .AddTo(Projects);
+        }
+
+        public void CreateClients(IEnumerable<IClient> clients)
+        {
+            clients
+                .Select(client => new Client(client))
+                .Select(client => new CreateAction<Client>(client))
+                .AddTo(Clients);
+        }
+
+        public void CreateTags(IEnumerable<ITag> tags)
+        {
+            tags
+                .Select(tag => new Tag(tag))
+                .Select(tag => new CreateAction<Tag>(tag))
+                .AddTo(Tags);
+        }
+
+        public void UpdatePreferences(IPreferences preferences)
+        {
+            var networkPreferences = new Preferences(preferences);
+            Preferences = new UpdateAction<IPreferences>(networkPreferences);
+        }
+
+        public void UpdateUser(IUser user)
+        {
+            var networkUser = new User(user);
+            User = new UpdateAction<IUser>(networkUser);
+        }
     }
 }
