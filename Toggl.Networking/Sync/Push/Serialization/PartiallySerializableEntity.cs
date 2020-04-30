5ebae6e29d64b7a37bb9cb540d46833efdab1f21
Kornelije Petak <kornelije@gmail.com>
2020-04-30 14:44:28 +0200

    🔀 Add partial entity serialization infrastructure

---
+++ Toggl.Networking/Sync/Push/Serialization/PartiallySerializableEntity.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Toggl.Networking.Models;
+using Toggl.Shared.Extensions;
+using Toggl.Shared.Models;
+
+namespace Toggl.Networking.Sync.Push.Serialization
+{
+    internal static class PartiallySerializableEntityFactory
+    {
+        public static IPartiallySerializableEntity Create<TEntity>(TEntity entity, TEntity backedUpEntity)
+        {
+            if (typeof(TEntity).ImplementsOrDerivesFrom(typeof(ITimeEntry)))
+            {
+                return new PartiallySerializableTimeEntry(entity as ITimeEntry, backedUpEntity as ITimeEntry);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Toggl.Networking/Sync/Push/Serialization/PartiallySerializableTimeEntry.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using Toggl.Networking.Models;
+using Toggl.Shared.Extensions;
+using Toggl.Shared.Models;
+
+namespace Toggl.Networking.Sync.Push.Serialization
+{
+    internal class PartiallySerializableTimeEntry : IPartiallySerializableEntity
+    {
+        public ITimeEntry Entity { get; private set; }
+        public ITimeEntry BackedUpEntity { get; private set; }
+
+        public PartiallySerializableTimeEntry(ITimeEntry entity, ITimeEntry backedUpEntity)
+        {
+            Entity = entity;
+            BackedUpEntity = backedUpEntity;
+        }
+
+        public JObject Serialize()
+        {
+            var obj = new JObject();
+
+            if (Entity.ProjectId != BackedUpEntity.ProjectId)
+                obj.Add(new JProperty("project_id", Entity.ProjectId));
+
+            if (Entity.TaskId != BackedUpEntity.TaskId)
+                obj.Add(new JProperty("task_id", Entity.TaskId));
+
+            if (Entity.Billable != BackedUpEntity.Billable)
+                obj.Add(new JProperty("billable", Entity.Billable));
+
+            if (Entity.Start != BackedUpEntity.Start)
+                obj.Add(new JProperty("start", Entity.Start));
+
+            if (Entity.Duration != BackedUpEntity.Duration)
+                obj.Add(new JProperty("duration", Entity.Duration));
+
+            if (Entity.Description != BackedUpEntity.Description)
+                obj.Add(new JProperty("description", Entity.Description));
+
+            var entityTags = Entity.TagIds ?? new long[0];
+            var backupEntityTags = BackedUpEntity.TagIds ?? new long[0];
+
+            if (!entityTags.SetEquals(backupEntityTags))
+            {
+                var array = new JArray();
+                entityTags.ForEach(tagId => array.Add(tagId));
+
+                obj.Add(new JProperty("tag_ids", array));
+            }
+
+            return obj;
+        }
+    }
+}
