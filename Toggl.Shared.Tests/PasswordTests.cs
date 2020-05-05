9e8bd57f27e8d77f4483b589481ae8a15df525df
pdrj <53510080+pdrj@users.noreply.github.com>
2020-05-05 10:20:35 +0200

    🔒 Add IsStrong property to Password (#7340)

--- Toggl.Shared.Tests/PasswordTests.cs
+++ Toggl.Shared.Tests/PasswordTests.cs
@@ -39,6 +39,74 @@ namespace Toggl.Shared.Tests
             }
         }
 
+        public sealed class TheIsStrongProperty
+        {
+            [Fact, LogIfTooSlow]
+            public void ReturnsFalseForEmptyOrNullPassword()
+            {
+                Password.Empty.IsStrong.Should().BeFalse();
+                Password.From(null).IsStrong.Should().BeFalse();
+            }
+
+            [Theory, LogIfTooSlow]
+            [InlineData("")]
+            [InlineData("1")]
+            [InlineData("pAs1s")]
+            [InlineData("1aB4567")]
+            [InlineData("    aff")]
+            [InlineData("\t axxX")]
+            public void ReturnsFalseWhenPasswordIsShorterThan8Characters(
+                string passwordString)
+            {
+                Password.From(passwordString).IsStrong.Should().BeFalse();
+            }
+
+            [Theory, LogIfTooSlow]
+            [InlineData("12345678999aa")]
+            [InlineData("fdasfsf333qwerty1")]
+            [InlineData("من أي وقت مضى الكلب القط من أي وقت مضىx11")]
+            [InlineData("####@!@!@!@!@!@!123aaa")]
+            public void ReturnsFalseWhenNoUppercasePresent(
+                string passwordString)
+            {
+                Password.From(passwordString).IsStrong.Should().BeFalse();
+            }
+
+            [Theory, LogIfTooSlow]
+            [InlineData("12345678999AA")]
+            [InlineData("SASASAS333SASQQQ1")]
+            [InlineData("111212121212121X")]
+            [InlineData("من أي وقت مضى الكلب القط من أي وقت مضى11X")]
+            [InlineData("####@!@!@!@!@!@!123AAA")]
+            public void ReturnsFalseWhenNoLowercasePresent(
+                string passwordString)
+            {
+                Password.From(passwordString).IsStrong.Should().BeFalse();
+            }
+
+            [Theory, LogIfTooSlow]
+            [InlineData("aaKASJLAJSAKLJLKA")]
+            [InlineData("fdLALSSKKSKSKS")]
+            [InlineData("من أي وقت مضى الكلب القط من أي وقت مضىxxXX")]
+            [InlineData("####@!@!@!@!@!@!aaAA")]
+            public void ReturnsFalseWhenNoNumberPresent(
+                string passwordString)
+            {
+                Password.From(passwordString).IsStrong.Should().BeFalse();
+            }
+
+            [Theory, LogIfTooSlow]
+            [InlineData("من أي وقت مضى الكلب القط من أي وقت مضى1xX")]
+            [InlineData("1Xxkldsjfklsdajfsdlk")]
+            [InlineData("####@!@!@!@!@!@!1xX")]
+            public void ReturnsTrueForPasswordsMatchingAllStrengthCriteria(
+                string passwordString)
+            {
+                Password.From(passwordString).IsStrong.Should().BeTrue();
+            }
+        }
+
+
         public sealed class TheEmptyProperty
         {
             [Fact, LogIfTooSlow]
