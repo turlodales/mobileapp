08ac8c027b11da26c8c8df719d25a07064591c7a
Ricardo Sánchez Sotres <odrakir@gmail.com>
2020-05-12 17:19:55 +0200

    Periscope eye animation (#7370)
    * Animate pupil
    
    * Animate eyelid

--- Toggl.iOS/Views/Onboarding/PeriscopeView.cs
+++ Toggl.iOS/Views/Onboarding/PeriscopeView.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
 using CoreAnimation;
 using CoreGraphics;
 using Foundation;
+using System.Threading;
 using UIKit;
 
 namespace Toggl.iOS
@@ -12,20 +12,28 @@ namespace Toggl.iOS
         private readonly CALayer periscopeLayer;
         private readonly CALayer eyeballLayer;
         private readonly CALayer pupilLayer;
+        private readonly CALayer eyelidLayer;
+        private readonly CAShapeLayer eyelidMask;
         private NSObject observer = null;
+
+        private Timer timer;
 
         public PeriscopeView()
         {
             periscopeLayer = new CALayer();
             eyeballLayer = new CALayer();
             pupilLayer = new CALayer();
+            eyelidLayer = new CALayer();
+            eyelidMask = new CAShapeLayer();
 
             periscopeLayer.Contents = UIImage.FromBundle("ic_periscope").CGImage;
             eyeballLayer.Contents = UIImage.FromBundle("ic_eyeball").CGImage;
             pupilLayer.Contents = UIImage.FromBundle("ic_pupil").CGImage;
+            eyelidLayer.Contents = UIImage.FromBundle("ic_eyelid").CGImage;
 
             Layer.AddSublayer(eyeballLayer);
             Layer.AddSublayer(pupilLayer);
+            Layer.AddSublayer(eyelidLayer);
             Layer.AddSublayer(periscopeLayer);
 
             observer = NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidBecomeActiveNotification, restartAnimation);
@@ -42,24 +50,64 @@ namespace Toggl.iOS
             // The magic numbers used here are to position the eyeball and pupil relative to the periscope image size
             periscopeLayer.Frame = Bounds;
             eyeballLayer.Frame = new CGRect(Bounds.Width * 0.571, Bounds.Height * 0.123, Bounds.Width * 0.203, Bounds.Height * 0.221);
+            eyelidLayer.Frame = new CGRect(Bounds.Width * 0.571, Bounds.Height * 0.123, Bounds.Width * 0.203, Bounds.Height * 0.221);
             pupilLayer.Frame = new CGRect(Bounds.Width * 0.730, Bounds.Height * 0.197, Bounds.Width * 0.110, Bounds.Height * 0.109);
+            pupilLayer.Position = new CGPoint(eyeballLayer.Frame.GetMidX(), eyeballLayer.Frame.GetMidY());
 
-            pupilLayer.RemoveAllAnimations();
+            eyelidMask.FillRule = CAShapeLayer.FillRuleEvenOdd;
+            eyelidMask.Path = CGPath.FromRect(eyelidLayer.Bounds);
 
-            var path = new UIBezierPath();
-            path.MoveTo(new CGPoint(Bounds.Width * 0.725, Bounds.Height * 0.238));
-            path.AddLineTo(new CGPoint(Bounds.Width * 0.628, Bounds.Height * 0.238));
-            path.AddArc(new CGPoint(Bounds.Width * 0.677, Bounds.Height * 0.238), (nfloat)(Bounds.Width * 0.049), 0, (nfloat)Math.PI, true);
-            path.ClosePath();
+            timer = new Timer(
+                (state) =>
+                    InvokeOnMainThread(() =>
+                        movePupil()
+                    ),
+                null,
+                0,
+                1000);
+        }
 
-            var animation = new CAKeyFrameAnimation();
-            animation.KeyPath = "position";
-            animation.Duration = 5;
-            animation.Path = path.CGPath;
-            animation.AutoReverses = true;
-            animation.RepeatCount = float.MaxValue;
+        private void movePupil()
+        {
+            var random = new Random();
 
-            pupilLayer.AddAnimation(animation, "animation");
+            // move eye
+            if (random.Next(0, 6) == 0)
+            {
+                var movementFrame = eyeballLayer.Frame;
+                movementFrame.Inflate( -movementFrame.Width / 3, -movementFrame.Height / 3);
+
+                var p = new CGPoint(
+                    movementFrame.X - pupilLayer.Frame.Width / 2 + random.Next(0, (int) movementFrame.Width),
+                    movementFrame.Y - pupilLayer.Frame.Height / 2 + random.Next(0, (int) movementFrame.Height)
+                );
+
+                pupilLayer.Frame = new CGRect(p, pupilLayer.Frame.Size);
+            }
+
+            // blink
+            if (random.Next(0, 10) == 0)
+            {
+                eyelidLayer.Mask = eyelidMask;
+                eyelidMask.RemoveAllAnimations();
+                var initialPosition = eyelidMask.Position;
+
+                var path = new UIBezierPath();
+                path.MoveTo(new CGPoint(initialPosition.X, initialPosition.Y - eyelidLayer.Frame.Height));
+                path.AddLineTo(initialPosition);
+                path.AddLineTo(new CGPoint(initialPosition.X, initialPosition.Y - eyelidLayer.Frame.Height));
+                path.ClosePath();
+
+                var animation = new CAKeyFrameAnimation();
+                animation.KeyPath = "position";
+                animation.Duration = 0.3;
+                animation.Path = path.CGPath;
+                animation.RepeatCount = random.Next(0, 5) == 0 ? 2 : 0;
+                animation.FillMode = CAFillMode.Forwards;
+                animation.RemovedOnCompletion = false;
+
+                eyelidMask.AddAnimation(animation, "animation");
+            }
         }
 
         protected override void Dispose(bool disposing)
@@ -68,6 +116,8 @@ namespace Toggl.iOS
 
             if (!disposing)
                 return;
+
+            timer.Dispose();
 
             if (observer != null)
             {
