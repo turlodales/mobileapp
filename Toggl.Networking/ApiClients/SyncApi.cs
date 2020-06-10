1a7c4adda2de10639dd06ec2ec95f14f2584b1af
Simon Rozsival <simon@rozsival.com>
2020-06-10 15:21:06 +0200

    :sparkles: Send custom header identifying the type of client (#7498)

--- Toggl.Networking/ApiClients/SyncApi.cs
+++ Toggl.Networking/ApiClients/SyncApi.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Toggl.Networking.Network;
 using Toggl.Networking.Serialization;
@@ -10,6 +11,7 @@ namespace Toggl.Networking.ApiClients
         private readonly SyncApiEndpoints endpoints;
         private readonly IApiClient apiClient;
         private readonly IJsonSerializer serializer;
+        private readonly IEnumerable<HttpHeader> headers; 
 
         public SyncApi(Endpoints endpoints, IApiClient apiClient, IJsonSerializer serializer, Credentials credentials)
             : base(apiClient, serializer, credentials, endpoints.LoggedIn)
@@ -17,18 +19,22 @@ namespace Toggl.Networking.ApiClients
             this.endpoints = endpoints.SyncServerEndpoints;
             this.apiClient = apiClient;
             this.serializer = serializer;
+
+            headers = new[] {
+                AuthHeader,
+                new HttpHeader("X-Toggl-Client", "mobile")
+            };
         }
 
         public async Task<Sync.Pull.IResponse> Pull(DateTimeOffset? since)
-            => await SendRequest<Sync.Pull.Response>(endpoints.Pull(since), AuthHeader);
+            => await SendRequest<Sync.Pull.Response>(endpoints.Pull(since), headers);
 
         public async Task<Sync.Push.IResponse> Push(Guid id, Sync.Push.Request request)
         {
             var body = serializer.Serialize(request, SerializationReason.Post);
-            return await SendRequest<Sync.Push.Response>(endpoints.Push(id), AuthHeader, body);
+            return await SendRequest<Sync.Push.Response>(endpoints.Push(id), headers, body);
         }
-
         public async Task<Sync.Push.IResponse> OutstandingPush(Guid id)
-            => await SendRequest<Sync.Push.Response>(endpoints.OutstandingPush(id), AuthHeader);
+            => await SendRequest<Sync.Push.Response>(endpoints.OutstandingPush(id), headers);
     }
 }
